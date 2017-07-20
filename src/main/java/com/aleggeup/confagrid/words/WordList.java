f7ca64f0fe2138967b3b177e90adc49bf675ac49
Stephen Legge <stephen@aleggeup.com>
2017-07-20 01:32:21 -0400

    WordList class to hold an ordered list of all words

---
+++ src/main/java/com/aleggeup/confagrid/words/WordList.java
@@ -0,0 +1,41 @@
+/*
+ * WordList.java
+ *
+ * Copyright (C) 2017 [ A Legge Up ] <stephen@aleggeup.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid.words;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordList
+{
+    private final List<String> words = new ArrayList<>();
+
+    public WordList() {
+    }
+
+    protected void addWordsFromPhrase(final Phrase phrase) {
+        final List<Word> wordsFromPhrase = phrase.getWords();
+
+        int lastWordIndex = -1;
+        for (final Word word : wordsFromPhrase) {
+            if (!words.contains(word.getWord()) && lastWordIndex > -1) {
+                words.add(++lastWordIndex, word.getWord());
+            } else if (!words.contains(word.getWord())) {
+                words.add(word.getWord());
+                lastWordIndex = words.indexOf(word.getWord());
+            } else {
+                lastWordIndex = words.indexOf(word.getWord());
+            }
+        }
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+}
---
+++ src/test/java/com/aleggeup/confagrid/words/WordListTest.java
@@ -0,0 +1,43 @@
+/*
+ * WordListTest.java
+ *
+ * Copyright (C) 2017 [ A Legge Up ] <stephen@aleggeup.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid.words;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class WordListTest
+{
+    private static final String PHRASE_STRING_1 = "IT IS ONE OCLOCK";
+    private static final String PHRASE_STRING_2 = "IT IS TWO OCLOCK";
+    private static final String PHRASE_STRING_3 = "IT IS THREE OCLOCK";
+
+    private static final Phrase PHRASE_1 = new Phrase(PHRASE_STRING_1);
+    private static final Phrase PHRASE_2 = new Phrase(PHRASE_STRING_2);
+    private static final Phrase PHRASE_3 = new Phrase(PHRASE_STRING_3);
+
+    @Test
+    public void simpleWordListTest() {
+        final WordList wordList = new WordList();
+        wordList.addWordsFromPhrase(PHRASE_1);
+        wordList.addWordsFromPhrase(PHRASE_2);
+        wordList.addWordsFromPhrase(PHRASE_3);
+        wordList.addWordsFromPhrase(PHRASE_3);
+        wordList.addWordsFromPhrase(PHRASE_2);
+        wordList.addWordsFromPhrase(PHRASE_1);
+
+        final List<String> words = new ArrayList<>(Arrays.asList("IT", "IS", "THREE", "TWO", "ONE", "OCLOCK"));
+        assertEquals(words, wordList.getWords());
+    }
+}
