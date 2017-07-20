e17f951e33fc2925b7907a97d19a0ce82d6f02ea
Stephen Legge <stephen@aleggeup.com>
2017-07-19 22:58:48 -0400

    Update junit version

--- src/test/java/com/aleggeup/confagrid/AppTest.java
+++ src/test/java/com/aleggeup/confagrid/AppTest.java
@@ -9,34 +9,19 @@
 
 package com.aleggeup.confagrid;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(final String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp() {
         assertTrue(true);
     }
