30e6de14261873942d95d7f0dd17a20b16519f67
Stephen Legge <stephen@aleggeup.com>
2017-07-19 22:30:13 -0400

    Initial generated files from quickstart archetype with cleanup

---
+++ src/main/java/com/aleggeup/confagrid/App.java
@@ -0,0 +1,21 @@
+/*
+ * App.java
+ *
+ * Copyright (C) 2017 [ A Legge Up ] <stephen@aleggeup.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
