a8d51d253effa10b519ceb8f952072d98b8543f3
Stephen Legge <stephen@aleggeup.com>
2017-08-01 03:18:49 -0400

    Start command line parser in main class

--- src/main/java/com/aleggeup/confagrid/App.java
+++ src/main/java/com/aleggeup/confagrid/App.java
@@ -9,13 +9,30 @@
 
 package com.aleggeup.confagrid;
 
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * Hello world!
  *
  */
-public class App
-{
+public class App {
+
+    private final static Option help = new Option("help", "print this message");
+
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        final Options options = new Options();
+        options.addOption(help);
+
+        final CommandLineParser parser = new DefaultParser();
+
+        try {
+            parser.parse(options, args);
+        } catch (final ParseException e) {
+            System.err.println("Parsing failed.  Reason: " + e.getMessage());
+        }
     }
 }
