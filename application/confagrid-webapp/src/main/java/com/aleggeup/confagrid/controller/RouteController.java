38841b42f7d60e3f22b01d4812d6eb0492af9bce
Stephen Legge <stephen@aleggeup.com>
2017-10-03 01:30:43 -0400

    Move RouteController to a common controller package

--- application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/controller/RouteController.java
+++ application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/controller/RouteController.java
@@ -1,4 +1,4 @@
-package com.aleggeup.confagrid.router;
+package com.aleggeup.confagrid.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
