8c096ffe3c97665dcda6c0b018ef7867758c2009
Stephen Legge <stephen@aleggeup.com>
2018-01-04 02:08:57 -0500

    Add missing header

--- application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/controller/RouteController.java
+++ application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/controller/RouteController.java
@@ -1,3 +1,12 @@
+/*
+ * RouteController.java
+ *
+ * Copyright (C) 2017 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 package com.aleggeup.confagrid.controller;
 
 import org.springframework.stereotype.Controller;
