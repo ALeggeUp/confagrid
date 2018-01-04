889261a6594d031a10a37fcf734db584afb2a6af
Stephen Legge <stephen@aleggeup.com>
2018-01-04 02:11:54 -0500

    Correct copyright header

--- application/confagrid-frontend/src/main/frontend/root/modules/main/views/login/login.component.ts
+++ application/confagrid-frontend/src/main/frontend/root/modules/main/views/login/login.component.ts
@@ -1,7 +1,7 @@
 /*
  * login.component.ts
  *
- * Copyright (C) 2017 Stephen Legge, Tim McCrabb and Andy Redfearn
+ * Copyright (C) 2017 [ A Legge Up ]
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
