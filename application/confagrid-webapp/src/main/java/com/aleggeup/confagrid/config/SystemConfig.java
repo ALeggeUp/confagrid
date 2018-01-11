87372e1d62c46554d6010c7e3c9d7ab2a3334dc3
Stephen Legge <stephen@aleggeup.com>
2018-01-11 16:23:56 -0500

    Add a configurer to add CORS header for development

---
+++ application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/config/SystemConfig.java
@@ -0,0 +1,29 @@
+/*
+ * SystemConfig.java
+ *
+ * Copyright (C) 2018 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SystemConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
