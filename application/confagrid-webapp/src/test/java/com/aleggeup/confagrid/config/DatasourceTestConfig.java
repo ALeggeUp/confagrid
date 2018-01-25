5329a27176074957578280457dd9bebae422327b
Stephen Legge <stephen@aleggeup.com>
2018-01-25 03:08:40 -0500

    Return a mock datasource for unit tests in 'test' profile

---
+++ application/confagrid-webapp/src/test/java/com/aleggeup/confagrid/config/DatasourceTestConfig.java
@@ -0,0 +1,29 @@
+/*
+ * DatasourceTestConfig.java
+ *
+ * Copyright (C) 2018 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid.config;
+
+import javax.sql.DataSource;
+
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+public class DatasourceTestConfig {
+
+    @Bean
+    @Primary
+    DataSource datasource() {
+        return Mockito.mock(DataSource.class);
+    }
+}
