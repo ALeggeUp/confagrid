69cd5fa7e69235798ac92a8334e072eac7f8a37d
Stephen Legge <stephen@aleggeup.com>
2018-01-25 03:07:58 -0500

    Create Mock application for test Spring environments

---
+++ application/confagrid-webapp/src/test/java/com/aleggeup/confagrid/MockApplication.java
@@ -0,0 +1,21 @@
+/*
+ * MockApplication.java
+ *
+ * Copyright (C) 2018 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(MockApplication.class, args);
+    }
+}
