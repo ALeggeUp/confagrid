0edbbfc22a76688b31b05450be314daa5416a299
Stephen Legge <stephen@aleggeup.com>
2018-01-28 00:01:12 -0500

    :white_check_mark: Add silly test for Route Controller to get coverage

--- application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/controller/RouteController.java
+++ application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/controller/RouteController.java
@@ -1,7 +1,7 @@
 /*
  * RouteController.java
  *
- * Copyright (C) 2017 [ A Legge Up ]
+ * Copyright (C) 2017-2018 [ A Legge Up ]
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RouteController {
 
+    protected static final String DEFAULT_ROUTE = "forward:/index.html";
+
     @RequestMapping({
         "/login",
         "/create",
@@ -23,6 +25,6 @@ public class RouteController {
         "/more"
     })
     public String router() {
-        return "forward:/index.html";
+        return DEFAULT_ROUTE;
     }
 }
---
+++ application/confagrid-webapp/src/test/java/com/aleggeup/confagrid/controller/RouteControllerTest.java
@@ -0,0 +1,23 @@
+/*
+ * RouteControllerTest.java
+ *
+ * Copyright (C) 2018 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RouteControllerTest {
+
+    @Test
+    public void testRouteController() {
+        final RouteController routeController = new RouteController();
+        assertEquals(RouteController.DEFAULT_ROUTE, routeController.router());
+    }
+}
