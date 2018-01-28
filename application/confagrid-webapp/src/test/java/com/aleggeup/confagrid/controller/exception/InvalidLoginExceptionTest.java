c4b081c67defb6b5fa5e1995fb6f6d07fe844e45
Stephen Legge <stephen@aleggeup.com>
2018-01-28 00:02:11 -0500

    :white_check_mark: Add silly test for empty exception to get coverage

---
+++ application/confagrid-webapp/src/test/java/com/aleggeup/confagrid/controller/exception/InvalidLoginExceptionTest.java
@@ -0,0 +1,23 @@
+/*
+ * InvalidLoginExceptionTest.java
+ *
+ * Copyright (C) 2018 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid.controller.exception;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class InvalidLoginExceptionTest {
+
+    @Test
+    public void basicTest() {
+        final InvalidLoginException invalidLoginException = new InvalidLoginException();
+        assertNotNull(invalidLoginException);
+    }
+}
