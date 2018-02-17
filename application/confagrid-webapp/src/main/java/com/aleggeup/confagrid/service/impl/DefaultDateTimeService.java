cab37ab9163486659e01d17d3c5b69545a0e913f
Stephen Legge <stephen@aleggeup.com>
2018-02-17 12:06:52 -0500

    Add date/time service

---
+++ application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/service/impl/DefaultDateTimeService.java
@@ -0,0 +1,41 @@
+/*
+ * DefaultDateTimeService.java
+ *
+ * Copyright (C) 2018 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid.service.impl;
+
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Service;
+
+import com.aleggeup.confagrid.service.DateTimeService;
+
+@Service
+public class DefaultDateTimeService implements DateTimeService {
+
+    @Override
+    public DateTime now() {
+        return DateTime.now();
+    }
+
+    @Override
+    public Date nowAsDate() {
+        return DateTime.now().toDate();
+    }
+
+    @Override
+    public DateTime plusDays(final DateTime dateTime, final int numberOfDays) {
+        return dateTime.plusDays(numberOfDays);
+    }
+
+    @Override
+    public Date plusDaysAsDate(final DateTime dateTime, final int numberOfDays) {
+        return dateTime.plusDays(numberOfDays).toDate();
+    }
+}
