e10ee794166cfbff304f432ca20489b84382d218
Stephen Legge <stephen@aleggeup.com>
2018-03-11 16:44:34 -0400

    Create initializers for words and phrases

---
+++ application/confagrid-webapp/src/main/java/com/aleggeup/confagrid/repository/init/WordRepositoryInitializer.java
@@ -0,0 +1,45 @@
+/*
+ * WordRepositoryInitializer.java
+ *
+ * Copyright (C) 2018 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package com.aleggeup.confagrid.repository.init;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.aleggeup.confagrid.repository.WordRepository;
+
+@Component
+public class WordRepositoryInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WordRepositoryInitializer.class);
+
+    private final WordRepository wordRepository;
+
+    @Autowired
+    public WordRepositoryInitializer(final WordRepository wordRepository) {
+        this.wordRepository = wordRepository;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        if (wordRepository.count() == 0) {
+            LOGGER.info("Word Repository is Being Initialized.");
+            initialize();
+        } else {
+            LOGGER.info("Word Repository is Already Initialized.");
+        }
+    }
+
+    private void initialize() {
+    }
+}
