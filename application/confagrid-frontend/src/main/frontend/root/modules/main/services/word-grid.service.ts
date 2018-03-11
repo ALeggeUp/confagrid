fe00214d7aa5aff5b60c70053b701216661e84ab
Stephen Legge <stephen@aleggeup.com>
2018-03-11 16:48:02 -0400

    New method in wordgrid service to get grid content

--- application/confagrid-frontend/src/main/frontend/root/modules/main/services/word-grid.service.ts
+++ application/confagrid-frontend/src/main/frontend/root/modules/main/services/word-grid.service.ts
@@ -8,15 +8,12 @@
  */
 
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, URLSearchParams, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 
 import { HttpService } from './http.service';
 
 import { WordGridModel } from '../models/word-grid.model';
-import { WordGridResponseItem } from '../models/word-grid-response-item.model';
+import { WordGridContentResponse } from '../models/mex/word-grid-content-response';
 
 @Injectable()
 export class WordGridService {
@@ -34,5 +31,9 @@ export class WordGridService {
 
     get(id: string): Observable<WordGridModel> {
         return this.httpService.getRequest<WordGridModel>('/api/v1/word-grid/' + id);
+    }
+
+    getContent(): Observable<WordGridContentResponse> {
+        return this.httpService.getRequest<WordGridContentResponse>('/api/v1/word-grid/content');
     }
 }
