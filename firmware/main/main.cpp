a21d63e9569f826fb0978d4057b280b4d675b5c2
Stephen Legge <stephen@aleggeup.com>
2018-06-03 03:15:04 -0400

    Try to create the animation and start it

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -9,6 +9,12 @@
 
 #include <stdio.h>
 
+#include "HardwareConfig.h"
+#include "AnimationTask.h"
+
 extern "C" void app_main()
 {
+    HardwareConfig hardwareConfig(HardwareConfig::DEV0);
+    AnimationTask *pAnimationTask = new AnimationTask("AnimationTask");
+    pAnimationTask->start();
 }
