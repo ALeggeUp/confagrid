2bc03746060069d9df48a1270d5c984c3c0f6ede
Stephen Legge <stephen@aleggeup.com>
2018-06-02 23:41:24 -0400

    Add license to header of main

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -1,7 +1,14 @@
+/*
+ * main.cpp
+ *
+ * Copyright (C) 2018 [ A Legge Up ]
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
 
 #include <stdio.h>
 
 extern "C" void app_main()
 {
 }
-
