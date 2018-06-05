e19ce1d6a42536c7eb7346ad9572ad939fe6981a
Stephen Legge <stephen@aleggeup.com>
2018-06-05 01:11:02 -0400

    Toolchain doesn't like block initialization

--- firmware/main/I2C.cpp
+++ firmware/main/I2C.cpp
@@ -21,13 +21,12 @@ I2C::~I2C() {
 esp_err_t I2C::init(i2c_port_t port_num, i2c_mode_t mode, gpio_num_t sda_io_num, gpio_num_t scl_io_num,
         gpio_pullup_t sda_pullup_en, gpio_pullup_t scl_pullup_en, uint32_t clk_speed) {
 
-    i2c_config_t conf = {
-        .mode = mode,
-        .sda_io_num = sda_io_num,
-        .scl_io_num = scl_io_num,
-        .sda_pullup_en = sda_pullup_en,
-        .scl_pullup_en = scl_pullup_en
-    };
+    i2c_config_t conf;
+    conf.mode = mode;
+    conf.sda_io_num = sda_io_num;
+    conf.scl_io_num = scl_io_num;
+    conf.sda_pullup_en = sda_pullup_en;
+    conf.scl_pullup_en = scl_pullup_en;
     conf.master.clk_speed = clk_speed;
 
     esp_err_t err = ::i2c_param_config(port_num, &conf);
